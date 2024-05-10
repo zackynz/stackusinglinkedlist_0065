edbcee035feadb9845d4cffc689f13dea0891d4f
zackynz <nurasfanz@gmail.com>
2024-05-10 16:19:19 +0700

    perbaikan code yang error

--- stackusinglinkedlist.cpp
+++ stackusinglinkedlist.cpp
@@ -1,116 +1,107 @@
 #include <iostream>
-
 using namespace std;
 
-
 class Node {
-public:
-    int data;
-    Node* next;
+    public:
+        int data;
+        Node* next;
 
-    Node() {
-        next = NULL;
-    }
+        Node() {
+            next = NULL;
+        }
 };
 
-
 class Stack {
-private:
-    Node* top; 
-
-public:
-    Stack () {
-        top = NULL; 
-    }
-
- 
-    int push(int value) {
-        Node* newNode = new Node();
-        newNode->data = value;
-        newNode->next = top;
-        top = newNode;
-        cout << "Push value: " << value << endl;
-        return value;
-    }
-    
-    
-    void pop() {
-    if (isEmpty()) {
-        cout << "Stack is empety." << endl;
-    }
-    cout << "Propped value: " << top->data << endl;
-    top = top->next;
-    
-    }
-
-
-    void peek() {
-        if (top == NULL) {
-            cout << "List is empty." << endl;
+    private:
+        Node* top;
+    public:
+        Stack() {
+            top = NULL;
         }
-        else {
-            Node* current = top;
-            while (current != NULL) {
-                cout << current->data << " " << endl;
-                current = current->next;
+        int push(int value) {
+            Node* newNode = new Node();
+            newNode->data = value;
+            newNode->next = top;
+            top = newNode;
+            cout << " push value: " << value << endl;
+            return value;
+        }
+
+        void pop() {
+            if (isEmpty()) {
+                cout << "Stack is empty." << endl;
+
             }
-            cout << endl;
+            cout << " Popped value: " << top->data << endl;
+            top = top->next;
         }
 
-
-
+        void peek() {
+            if (top == NULL) {
+                cout << " List is empty" << endl;
+            }
+            else {
+                Node* current = top;
+                while (current != NULL) {
+                    cout << current->data << " " << endl;
+                    current = current->next;
+                }
+                cout << endl;
+            }
+        }
         bool isEmpty() {
             return top == NULL;
         }
-    }
 };
 
-int main() {
+
+int main()
+{
     Stack stack;
 
     int choice = 0;
     int value;
 
     while (choice != 5) {
-        cout << "1. Push\n";
-        cout << "2. Pop\n";
-        cout << "3. Peek\n";
-        cout << "4. Exit\n";
-        cout << "Enter your choice: ";
+        cout << "1.Push\n";
+        cout << "2.pop\n";
+        cout << "3.peek";
+        cout << "4.Exit";
+        cout << " Enter your choice";
         cin >> choice;
-
-        switch (choice) {
-        case 1:
-            cout << "Enter the value to push: ";
-            cin >> value;
-            stack.push(value);
-            break;
-        case 2:
-            if (!stack.isEmpty()) {
-                stack.pop();
-            }
-            else {
-                cout << "Stack is empty. Cannot pop." << endl;
-            }
-            break;
-        case 3:
-            if (!stack.isEmpty()) {
-                stack.peek();
-            }
-            else {
-                cout << "Stack is empty. No top value." << endl;
-            }
-            break;
-        case 4:
-            cout << "Exiting program." << endl;
-            break;
-        default:
-        cout << "Invalid choice. Try again." << endl;
-        break;
-        }
-
-        cout << endl;
     }
+    switch (choice) {
+    case 1:
+        cout << "Enter the value to push: ";
+        cin >> value;
+        stack.push(value);
+        break;
+    case 2:
+        if (!stack.isEmpty()) {
+            stack.pop();
+        }
+        else {
+            cout << "Stack is empty.cannot pop" << endl;
+        }
+        break;
+    case 3:
+        if (!stack.isEmpty()) {
+            stack.peek();
+        }
+        else {
+            cout << " Stack is empty. No top value." << endl;
 
+        }
+        break;
+    case 4:
+        cout << " Exiting program " << endl;
+        break;
+    default:
+        cout << "invalid choice. try again" << endl;
+        break;
+
+    }
+    cout << endl;
     return 0;
+
 }
