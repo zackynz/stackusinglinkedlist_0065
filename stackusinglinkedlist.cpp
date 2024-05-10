318be4a45b80d3eb294073e58c418cfb4a856bc9
zackynz <nurasfanz@gmail.com>
2024-05-10 16:05:52 +0700

    menggunakan switch untuk memilih salah satu block code

--- stackusinglinkedlist.cpp
+++ stackusinglinkedlist.cpp
@@ -71,5 +71,16 @@ int main() {
     int choice = 0;
     int value;
 
-    while (choice != 5)
+    while (choice != 5) {
+        cout << "1. Push\n";
+        cout << "2. Pop\n";
+        cout << "3. Peek\n";
+        cout << "4. Exit\n";
+        cout << "Enter your choice: ";
+        cin >> choice;
+
+        switch (choice) {
+            
+        }
+    }
 }
